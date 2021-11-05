54ce2156be93b2cfdb60bd7ce889c080cdb0260b
Andrew Cummings <andrew.amalee@gmail.com>
2021-11-05 16:54:12 -0700

    contact forms done. need to work on mobile view

--- midwestern-tech-challenge-app/src/components/Contact.js
+++ midwestern-tech-challenge-app/src/components/Contact.js
@@ -3,6 +3,7 @@ import logo from "../assets/logo.png";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import Form from "./Form";
+import "../Form.css";
 
 const initialFormValues = {
   firstName: "",
@@ -39,30 +40,39 @@ const Contact = () => {
   return (
     <div>
       <header>
-        <nav>
-          <img src={logo} class="Logo" />
-          <Link to="/">home</Link>
-        </nav>
+        <nav></nav>
       </header>
-      <div class="contact-heading-one">
-        <div class="contact-heading-one-title">Heading One</div>
-        <div class="contact-heading-one-info">
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
-            aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis
-            suscipit eaque?
-          </p>
+      <div class="contact-body">
+        <div class="contact-heading-one">
+          <img src={logo} class="Logo" />
+          <div class="contact-heading-one-title">Heading One</div>
+          <div class="contact-hr">
+            <hr />
+          </div>
+          <div class="contact-heading-one-info">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Molestias aut, repellat ipsum facere voluptate dicta obcaecati
+              deserunt nobis suscipit eaque?
+            </p>
+          </div>
         </div>
-      </div>
-      <div class="contact-heading-two">
-        <div class="contact-heading-two-title">Heading Two</div>
-        <div class="contact-form">
-          <Form
-            values={formValues}
-            update={updateForm}
-            submit={submitForm}
-            change={inputChange}
-          />
+        <div class="contact-heading-two">
+          <Link to="/" class="home-link">
+            home
+          </Link>
+          <div class="contact-heading-two-title">Heading Two</div>
+          <div class="contact-form">
+            <Form
+              values={formValues}
+              update={updateForm}
+              submit={submitForm}
+              change={inputChange}
+            />
+          </div>
+          <div class="submit-btn">
+            <button>Submit</button>
+          </div>
         </div>
       </div>
     </div>
