6f85b1da764d9851ee792b4a6f04b8281da96a30
Andrew Cummings <andrew.amalee@gmail.com>
2021-11-04 23:51:49 -0700

    Contact page route working

--- midwestern-tech-challenge-app/src/App.js
+++ midwestern-tech-challenge-app/src/App.js
@@ -1,82 +1,36 @@
+import React, { useState } from "react";
 import logo from "./assets/logo.png";
-import rabbit from "./assets/Rabbit.png";
-import shield from "./assets/Shield.png";
-import talkie from "./assets/Talkie.png";
+import { Routes, Route, Link } from "react-router-dom";
+import Contact from "./components/Contact";
+import Home from "./components/Home";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const initialFormValues = {
+    firstName: "",
+    lastName: "",
+    title: "",
+    email: "",
+    message: "",
+  };
+  // const [formValues, setFormValues] = useState(initialFormValues);
+
   return (
-    <body>
+    <div>
       <header>
         <nav>
-          <img src={logo} class="Logo" />
-          <a class="contact">contact</a>
+          <Link to="/">
+            <img src={logo} class="Logo" />
+          </Link>
+          <Link to="/contact">contact</Link>
         </nav>
       </header>
-      <div class="heading-container">
-        <div class="heading1">
-          <img src={talkie} id="talkie" alt="picture of a microphone" />
-          <div class="heading-title">Heading Two</div>
-          <div class="heading-info">
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-              animi modi voluptates sed nemo mollitia vitae corrupti maiores.
-              Sapiente commodi ad expedita, vero minus iure quos. Quia delectus
-              aliquid officia.
-            </p>
-          </div>
-          <div class="btn-holder">
-            <button>Learn More</button>
-          </div>
-        </div>
-        <div class="heading2">
-          <img src={rabbit} id="rabbit" alt="picture of a rabbit" />
-          <div class="heading-title">Heading Two</div>
-          <div class="heading-info">
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-              animi modi voluptates sed nemo mollitia vitae corrupti maiores.
-              Sapiente commodi ad expedita, vero minus iure quos. Quia delectus
-              aliquid officia.
-            </p>
-          </div>
-          <div class="btn-holder">
-            <button>Learn More</button>
-          </div>
-        </div>
-        <div class="heading3">
-          <img src={shield} id="shield" alt="picture of a shield" />
-          <div class="heading-title">Heading Two</div>
-          <div class="heading-info">
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi
-              animi modi voluptates sed nemo mollitia vitae corrupti maiores.
-              Sapiente commodi ad expedita, vero minus iure quos. Quia delectus
-              aliquid officia.
-            </p>
-          </div>
-          <div class="btn-holder">
-            <button>Learn More</button>
-          </div>
-        </div>
-      </div>
-      <section class="page-bottom">
-        <div class="bottom-title">
-          <h1>Heading One</h1>
-        </div>
-        <div class="hr">
-          <hr />
-        </div>
-        <div class="bottom-text">
-          Remove the duplicates in 2 Javascript objects and output the list of
-          distinct names in an unordered list when
-          <a onclick="console.log(here)">this link</a> is clicked. If the
-          operation has been completed already notify the user that this has
-          already been done.
-        </div>
-      </section>
-    </body>
+      <Routes>
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ midwestern-tech-challenge-app/src/components/Contact.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h2>Hello World</h2>
+    </div>
+  );
+}
+export default Contact;
